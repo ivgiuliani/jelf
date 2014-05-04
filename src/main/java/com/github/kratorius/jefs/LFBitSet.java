4f8f5d0afb32012f84f53ec4425458e38921ea5d
Ivan Giuliani <giuliani.v@gmail.com>
2014-05-05 00:45:01 +0200

    Include javadoc on LFBitSet

--- src/main/java/com/github/kratorius/jefs/LFBitSet.java
+++ src/main/java/com/github/kratorius/jefs/LFBitSet.java
@@ -2,6 +2,11 @@ package com.github.kratorius.jefs;
 
 import sun.misc.Unsafe;
 
+/**
+ * Represents a vector of a fixed number of bits.
+ * This class differs from the normal {@link java.util.BitSet} class in that we only allow
+ * a fixed number of bits.
+ */
 public class LFBitSet extends NotSafe {
   private static Unsafe unsafe = getUnsafe();
   private static final int base = unsafe.arrayBaseOffset(long[].class);
@@ -16,6 +21,11 @@ public class LFBitSet extends NotSafe {
   private volatile long[] bitset;
   private final int nbits;
 
+  /**
+   * Creates a new bit set whose size is large enough to explicitely represent bits
+   * with indices in the range {@code 0} through {@code nbits - 1}.
+   * @param nbits the initial size of the bit set
+   */
   public LFBitSet(int nbits) {
     if (nbits < 0) {
       throw new IllegalArgumentException();
@@ -34,10 +44,17 @@ public class LFBitSet extends NotSafe {
     return ((bit - 1) >> 6) + 1;
   }
 
+  /**
+   * Sets all the bits in this bit set to {@code false}.
+   */
   public void clear() {
     this.bitset = new long[getBucket(nbits) + 1];
   }
 
+  /**
+   * Sets the bit specified by the index to false.
+   * @param bitIndex the index of the bit to be cleared
+   */
   public void clear(int bitIndex) {
     int bucket = getBucket(bitIndex);
     if (bucket < 0 || bucket >= bitset.length) {
@@ -54,6 +71,10 @@ public class LFBitSet extends NotSafe {
     }
   }
 
+  /**
+   * Sets the bit at the specified index to the complement of its current value.
+   * @param bitIndex the index of the bit to flip
+   */
   public void flip(int bitIndex) {
     int bucket = getBucket(bitIndex);
     if (bucket < 0 || bucket >= bitset.length) {
@@ -70,6 +91,10 @@ public class LFBitSet extends NotSafe {
     }
   }
 
+  /**
+   * Sets the bit at the specified index to true.
+   * @param bitIndex a bit index
+   */
   public void set(int bitIndex) {
     int bucket = getBucket(bitIndex);
     if (bucket < 0 || bucket >= bitset.length) {
@@ -86,6 +111,11 @@ public class LFBitSet extends NotSafe {
     }
   }
 
+  /**
+   * Sets the bit at the specified index to the specified value.
+   * @param bitIndex a bit index
+   * @param value    a boolean value to set
+   */
   public void set(int bitIndex, boolean value) {
     if (value) {
       set(bitIndex);
@@ -94,6 +124,15 @@ public class LFBitSet extends NotSafe {
     }
   }
 
+  /**
+   * Returns the value of the bit with the specified index.
+   * The value is true if the bit with the index {@code bitIndex} is currently set
+   * in this BitSet; otherwise, the result is {@code false}.
+   * @param bitIndex a bit index
+   * @return the value of the bit with the specified index
+   * @throws java.lang.IndexOutOfBoundsException if the specified index is negative or
+   *         exceeds the bit set length
+   */
   public boolean get(int bitIndex) {
     int bucket = getBucket(bitIndex);
     if (bucket < 0 || bucket >= bitset.length) {
